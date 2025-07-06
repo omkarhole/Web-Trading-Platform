f7afb2eb90cbfbda4248654182cd6bbbdde30a12
Omkar Hole <omkarhole314@gmail.com>
2025-07-06 17:59:37 +0530

    Update Signup.jsx

--- dashboard/src/pages/Signup.jsx
+++ dashboard/src/pages/Signup.jsx
@@ -32,7 +32,7 @@ const Signup = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "http://localhost:3005/signup",
+        "https://zerodha-backend-9buv.onrender.com/signup",
         {
           ...inputValue,
         },
