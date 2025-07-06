9e16b18526cc5a5dfccba770ae5c3e0b6de6a595
Omkar Hole <omkarhole314@gmail.com>
2025-07-06 17:57:21 +0530

    Update Holdings.jsx

--- dashboard/src/components/Holdings.jsx
+++ dashboard/src/components/Holdings.jsx
@@ -6,7 +6,7 @@ const Holdings = () => {
   const [allHoldings, setAllHoldings] = useState([]);
 
   useEffect(() => {
-    axios.get("http://localhost:3005/allHoldings").then((res) => {
+    axios.get("https://zerodha-backend-9buv.onrender.com/allHoldings").then((res) => {
       console.log(res.data);
       setAllHoldings(res.data);
     })
