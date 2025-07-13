db1b6d35d38b927c57a535538523c0167b06a3c7
Omkar Hole <omkarhole314@gmail.com>
2025-07-13 20:00:39 +0530

    Update Home.jsx
    remove authentication due to error

--- dashboard/src/components/Home.jsx
+++ dashboard/src/components/Home.jsx
@@ -1,9 +1,9 @@
 //authentication imports 
-import React, { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { useCookies } from "react-cookie";
-import axios from "axios";
-import { ToastContainer, toast } from "react-toastify";
+// import React, { useEffect, useState } from "react";
+// import { useNavigate } from "react-router-dom";
+// import { useCookies } from "react-cookie";
+// import axios from "axios";
+// import { ToastContainer, toast } from "react-toastify";
 //imports 
 import TopBar from "./TopBar";
 import Dashboard from "./Dashboard";
